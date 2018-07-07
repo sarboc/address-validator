80097de89273288d1238c74dd5ddae26fbbba8b7
Sara Bocciardi <sjbocciardi@gmail.com>
2018-07-06 19:47:40 -0700

    Alert user when address is saved/not saved

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -8,7 +8,17 @@ class AddressesController < ApplicationController
   def create
     parsed_address = AddressParsingService.new(address_search_string).parsed_address
     @address = Address.new(parsed_address)
-    @address.save
+
+    if @address.save
+      @message = 'address was saved!'
+      @street_address, @city, @state, @zip_code = nil
+    else
+      @message = 'fix that address!'
+      @street_address = address_params[:street_address]
+      @city = address_params[:city]
+      @state = address_params[:state]
+      @zip_code = address_params[:zip_code]
+    end
 
     render 'new'
   end
